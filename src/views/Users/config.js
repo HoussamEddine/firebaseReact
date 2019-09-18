bd0a52c218542bc3b9bfd7dce5a8a4558c9dabac
Houssam-Eddine <chetouanihoussameddine@gmail.com>
2019-09-18 02:58:43 +0200

    changing gitignore

--- src/views/Users/config.js
+++
@@ -1,5 +0,0 @@
- export default{
-    apiKey: "AIzaSyCqZdb9kAcU83wlug0GMl8Pq4HJU50S-_Q",
-    authDomain: "novelis-54e89.firebaseapp.com",
-    databaseURL: "https://novelis-54e89.firebaseio.com",
-  };
